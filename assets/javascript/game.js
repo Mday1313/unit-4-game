402149bae69ad7010f51080a6a2ceaa7891035cd
Mday1313 <melissaday076@gmail.com>
2019-03-18 08:20:59 -0400

    html added and js psuedocoded

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+
+
+
+
+
+// Function to wrap all 
+
+
+
+
+// create variables
+
+var targetNumber;
+
+var crystalWorth = [];
+// counter store and adds user clicks 
+var counter;    
+
+
+
+// function that picks numbers 
+        // randomly pick number to match (TARGET NUMBER) limit 19-120
+
+        // for each crystal,
+                // randomly pick different number for each crystal, value between 1-12
+
+
+// function to clear display
+        // clears targetNumber and guess array
+
+// function to reset the game, calls on all the function necessary to begin
+    // clear
+    // pick numbers
+
+
+// function for click event
+        // lines 63 on in 12.html activity simple crystal game
+
+
+
+
+
+        // counter += crystalValue;
+
+    // if (counter === targetNumber) {
+        // write "U win"
+        // wins++
+        // reset()
+    // }
+        // else if (counter >= targetNumber) {
+            // write "U lose"
+            // losses--
+            // reset()
+        // }
